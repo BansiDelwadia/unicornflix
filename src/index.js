c4756c3d66a9ddf1c71f79eaf8ba680856991ebf
Bansi Delwadia <18ce019@charusat.edu.in>
2021-02-10 21:48:27 +0530

    VOD application

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-env browser */
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -6,7 +7,10 @@ import * as serviceWorker from './serviceWorker';
 import Routing from './components/Router';
 // Insert Location 1
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 
+Amplify.configure(awsconfig);
 ReactDOM.render(Routing, document.getElementById('root'));
 
 serviceWorker.unregister();
